b8f752b4c249bd350fb7228c83b3a87213a472a4
SIMONIN Richard <Richard.SIMONIN@tcs.ch>
2025-02-27 12:21:49 +0100

    add multithreading to reduce time

--- src/ace_exporter.py
+++ src/ace_exporter.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime, timedelta
 from flask import Flask, Response
 from api_client import APIClient
@@ -11,7 +12,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 app = Flask(__name__)
 api_client = APIClient(
-    'http://localhost:4414',
+    os.environ.get('ACE_URL'),
     os.environ.get('ACE_USERNAME'),
     os.environ.get('ACE_PASSWORD')
 )
@@ -31,27 +32,39 @@ def update_metrics():
         logging.info(f"Début du processus de mise à jour des métriques à {start_time}")
         
         structure = ace_structure.get_structure()
-        for server_name, server_data in structure.items():
-            for app_name, app_data in server_data['applications'].items():
-                for flow in app_data['message_flows']:
-                    flow_details = api_client.fetch_uri(flow['uri'])
-                    if flow_details:
-                        active_data = flow_details['active']
-                        metrics_handler.update_thread_metrics(
-                            server_name,
-                            app_name,
-                            flow['name'],
-                            active_data.get('threads', 0),
-                            active_data.get('threadsCapacity', 0),
-                            active_data.get('threadsDemanded', 0),
-                            active_data.get('threadsInUse', 0)
-                        )
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            futures = []
+            for server_name, server_data in structure.items():
+                for app_name, app_data in server_data['applications'].items():
+                    for flow in app_data['message_flows']:
+                        futures.append(executor.submit(process_flow_metrics, api_client, server_name, app_name, flow))
+            
+            for future in as_completed(futures):
+                try:
+                    future.result()
+                except Exception as exc:
+                    logging.error(f"Une erreur s'est produite lors de la mise à jour des métriques : {exc}")
         
         end_time = datetime.now()
         duration = end_time - start_time
         logging.info(f"Fin du processus de mise à jour des métriques à {end_time}. Durée : {duration}")
         
         time.sleep(15)  # Mise à jour toutes les 15 secondes
+
+def process_flow_metrics(api_client, server_name, app_name, flow):
+    flow_details = api_client.fetch_uri(flow['uri'])
+    if flow_details:
+        active_data = flow_details['active']
+        metrics_handler.update_thread_metrics(
+            server_name,
+            app_name,
+            flow['name'],
+            active_data.get('threads', 0),
+            active_data.get('threadsCapacity', 0),
+            active_data.get('threadsDemanded', 0),
+            active_data.get('threadsInUse', 0)
+        )
+
 
 @app.route('/metrics')
 def metrics():
